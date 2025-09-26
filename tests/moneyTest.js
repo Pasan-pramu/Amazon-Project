1bda3cdc9dc0aa4f8b774e8d6994be1927c1d933
Pasan24 <pramudithapasan076@gmail.com>
2025-09-26 14:40:08 +0530

    create automated  test for format currency

---
+++ tests/moneyTest.js
@@ -0,0 +1,23 @@
+import {formatCurrency} from "../scripts/utils/money.js";
+
+console.log('convert cents into  dollors ')
+
+if( formatCurrency(2095)==='20.95' ){
+    console.log('passed');
+}else{
+    console.log('failed');
+}
+
+console.log('works with zero ')
+if(formatCurrency(0)==='0.00'){
+       console.log('passed');
+}else{
+    console.log('failed');
+}
+
+console.log('round up to the nearest cents')
+if(formatCurrency(2000.5)==='20.01'){
+       console.log('passed');
+}else{
+    console.log('failed');
+}
